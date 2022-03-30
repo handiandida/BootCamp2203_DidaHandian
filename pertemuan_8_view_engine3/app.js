9c4ec68cb7655569fab77d209b2ce49da38b4f95
handiandida@gmail.com <handiandida@gmail.com>
2022-03-30 15:57:33 +0700

    latihan view engine array empty EJS

---
+++ pertemuan_8_view_engine3/app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const { getMaxListeners } = require('process')
+
+
+//informasi menggunkan ejs
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    cont = [
+        // {
+        //     nama : "Dida",
+        //     email : "dida@gmail.com"
+        // },
+        // {
+        //     nama : "Handian",
+        //     email : "handian@gmail.com"
+        // },
+        // {
+        //     nama : "Dizda",
+        //     email : "dizda@gmail.com"
+        // },
+    ]
+    res.render('index', 
+    { 
+        nama : "Hanya Dida yang Tampan", 
+        title : "Web EJS",
+        cont
+    }
+    )
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', 
+    { 
+        about : "Latihan EJS", 
+        title : "Web EJS"
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', 
+    { 
+        contact : "Latihan EJS", 
+        title :"Web EJS",
+    })
+})
+
+app.get('/product/:id', (req, res) => {
+    res.send('product id : '+ req.params.id +'<br></br>' + 'category id : ' + req.query.category)
+})
+
+
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send('page not found 404')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
