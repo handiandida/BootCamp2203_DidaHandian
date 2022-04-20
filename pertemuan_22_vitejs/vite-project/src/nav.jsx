8a3d90c3b7c0a9c0f7e7056357302b1c7835fbd5
handiandida@gmail.com <handiandida@gmail.com>
2022-04-20 14:52:58 +0700

    latihan vitejs with bootstrap

--- pertemuan_22_vitejs/vite-project/src/nav.jsx
+++ pertemuan_22_vitejs/vite-project/src/nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../img/logowgs.png'
 
 class nav extends React.Component {
     constructor(props) {
@@ -29,7 +30,16 @@ class nav extends React.Component {
     <React.Fragment>
         <Navbar bg="primary" expand="lg">
         <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="#">
+            <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+                />{' '}
+            React Bootstrap
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -38,7 +48,7 @@ class nav extends React.Component {
             </Nav>
             <Nav>
                 <Nav>
-                {new Date().toLocaleTimeString()}
+                <strong>{new Date().toLocaleTimeString()}</strong>
                 </Nav>
             </Nav>
             </Navbar.Collapse>
