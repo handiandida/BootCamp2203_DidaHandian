e6ae437efbf52c0f72f3c468ad01fb82f3acea24
handiandida@gmail.com <handiandida@gmail.com>
2022-04-21 10:16:40 +0700

    latihan vitejs contacts js

---
+++ pertemuan_23_vitejs_contacts/vite-project/src/contact.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name : "Dida",
+        email : "dida@gmail.com",
+        mobile : "081318181818"
+    },
+    {
+        name : "Dizda",
+        email : "dizda@gmail.com",
+        mobile : "081318182020"
+    },
+    {
+        name : "Handian",
+        email : "handian@gmail.com",
+        mobile : "081318181919"
+    }
+]
